b872a24571036956eab8267ada4c1a2d6095b70a
Ubuntu <ubuntu@ip-172-31-45-141.ap-south-1.compute.internal>
2024-07-21 08:30:59 +0000

    Add First Shell Script

---
+++ Day01/first.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#This is first Shell Script.
+
+
+echo "Hello Frieds-- Hope you are well!"
+
+echo "what is git?"
+
+echo "Git is a version control system that allows you to track changes to your files and collaborate with others. It is used to manage the history of your code and to merge changes from different branches."
+
+echo "what is github?"
+
+echo "Github is a web-based hosting service for Git repositories. Github is an online platform that allows you to store and share your code with others."
+
