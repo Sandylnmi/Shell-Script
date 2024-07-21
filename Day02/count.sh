6ada3979831e4459d406f41ba3c1fe676a7f9746
Ubuntu <ubuntu@ip-172-31-45-141.ap-south-1.compute.internal>
2024-07-21 08:56:04 +0000

    Add Day02 Shell Script

---
+++ Day02/count.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## Assigning a value to a variable
+name="Ram Kumar"
+age=21
+echo $name $age
+
+# Ask the user for their name
+ echo “What’s your name?”
+ read name
+# # Greet the user
+echo “Hello, $name! Nice to meet you.”
+
